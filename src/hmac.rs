0c00c0ea2e5783c528a395c2128c16065d9181f4
Alex Laurent <shaoner@pm.me>
2021-09-23 21:08:09 -0400

    Update doc in hmac module

--- src/hmac.rs
+++ src/hmac.rs
@@ -11,9 +11,10 @@ pub fn hash256(msg: &[u8]) -> [u8; 32] {
     Sha256::digest(&d1).as_slice().try_into().expect("digest should be 32 bytes")
 }
 
-pub fn hmac256(k: &[u8; 32], msg: &[u8]) -> [u8; 32] {
+/// hash buffer with secret key k
+pub fn hmac256(k: &[u8; 32], buf: &[u8]) -> [u8; 32] {
     let mut hm = HmacSha256::new_from_slice(k).unwrap();
-    hm.update(msg);
+    hm.update(buf);
 
     hm.finalize().into_bytes().try_into().expect("hmac should be 32 bytes")
 }
