760ba0aeb5edc816dc2a1c5c8c66198529955c69
Alex Laurent <shaoner@pm.me>
2021-09-26 20:25:52 -0400

    Re-export key functions by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+mod ecc;
+mod field;
 mod hmac;
+mod key;
+mod scalar;
 
-pub mod ecc;
-pub mod field;
-pub mod key;
-pub mod scalar;
+pub use crate::hmac::hash256;
+pub use crate::key::{PrivateKey, PublicKey, Signature};
+pub use crate::scalar::Scalar;
