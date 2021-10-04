407b147bc98b3c90960037d953cfe4c2885002d6
Alex Laurent <shaoner@pm.me>
2021-10-04 18:45:04 -0400

    Authorizes else { if { branches in linter

--- lint.sh
+++ lint.sh
@@ -4,4 +4,5 @@ cargo clippy -- \
       -A clippy::identity_op \
       -A clippy::branches_sharing_code \
       -A clippy::wrong-self-convention \
+      -A clippy::collapsible-else-if \
       -D warnings
